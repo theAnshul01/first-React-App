ab4c5242f971abec580df38dcd15eb7432e0bbf0
Anshul <anshulporwal2001@gmail.com>
2023-08-14 18:48:41 +0530

    first commit after adding dark mode

--- src/App.js
+++ src/App.js
@@ -1,24 +1,28 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
+import Navbar from './components/Navbar';
+import TextForm from './components/TextForm';
 
 function App() {
+  const [mode , setMode] =useState('light');  //whether dark mode is enabled or not
+  const toggleMode = ()=>{
+    if(mode === 'light'){
+      setMode('dark');
+      document.body.style.backgroundColor = '#23272f';
+    }else{
+      setMode('light');
+      document.body.style.backgroundColor = 'white';
+    }
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Navbar title="TextUtils" mode={mode} toggleMode={toggleMode}/>
+      <div className="container">
+        <TextForm heading="Enter the text below: " mode={mode}/>
+      </div>
+    </>
+    
   );
 }
 
---
+++ src/components/TextForm.js
@@ -0,0 +1,45 @@
+import React , {useState} from 'react'
+
+export default function TextForm(props) {
+    const handleUpClick = ()=>{
+        // console.log("uppercase button clicked");
+        let newtext = text.toUpperCase();
+        setText(newtext);
+    }
+    const handleLowClick = ()=>{
+        // console.log("uppercase button clicked");
+        let newtext = text.toLowerCase();
+        setText(newtext);
+    }
+    const handleOnChange = (event)=>{
+        // console.log("handling on change");
+        setText(event.target.value);
+    }
+    const clearText = (event)=>{
+        setText("");
+    }
+    const [text, setText] = useState("");
+  return (
+    <>
+    <div className='container' style={{color : props.mode==='light'?'black':'white'}}>
+        <h1 className='my-1'>{props.heading}</h1>
+        <div className="mb-3 my-1">
+            {/* <label for="myBox" className="form-label"></label> */}
+            <textarea className="form-control" value={text} onChange={handleOnChange} id="myBox" rows="8" style={{backgroundColor: props.mode==='light'?'white' : '#23272f' , 
+        color: props.mode==='light'?'black':'white'}}></textarea>
+            <button className='btn btn-warning my-2 ' onClick={clearText}> Clear text</button>
+            <button className="btn btn-primary my-2 mx-2" onClick={handleUpClick}>Convert to Uppercase</button>
+            <button className="btn btn-primary my-2" onClick={handleLowClick}>Convert to Lowercase</button>
+        </div>
+    </div>
+    <div className="container my-3" style={{color : props.mode==='light'?'black':'white'}}>
+        <h2>Your text summary</h2>
+        <p>{text.length>0?text.split(" ").length:"0"} words and {text.length} characters </p>
+        <p>{0.008 * text.split(" ").length } minutes to read</p>
+        
+        <h2>Preview</h2>
+        <p>{text.length>0?text:"Enter something in the above textbox to preview"}</p>
+    </div>
+    </>
+  )
+}
