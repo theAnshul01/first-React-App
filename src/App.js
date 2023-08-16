db120f13cef4f2fc971a98c28348b27e4236e4e2
Anshul <anshulporwal2001@gmail.com>
2023-08-16 19:51:22 +0530

    adding favicon and title

--- src/App.js
+++ src/App.js
@@ -25,10 +25,12 @@ function App() {
       setMode('dark');
       document.body.style.backgroundColor = '#23272f';
       showAlert('Dark mode has been enabled' , 'success');
+      // document.title = "TextUtils - Dark Mode"
     }else{
       setMode('light');
       document.body.style.backgroundColor = 'white';
       showAlert('Light mode has been enabled' , 'success');
+      // document.title = "TextUtils - Light Mode"
     }
   }
   return (
